09b292190dc0a3958b6031fc87e85e83b56116ac
Mark Lenser <mlenser@gmail.com>
2025-05-16 09:57:42 +0100

    refactor: split tokens-schema

--- schema/tokens-schema.ts
+++ schema/tokens-schema.ts
@@ -14,25 +14,28 @@ import {
 
 import { AddressSchema } from './address-schema'
 
+export const DefaultListBasicTokenSchema = strictObject({
+  address: AddressSchema,
+  decimals: number(),
+  image: string(),
+  name: string(),
+  symbol: string(),
+})
+export const DefaultListTokenWithUnderlyingSchema = strictObject({
+  address: DefaultListBasicTokenSchema.entries.address,
+  decimals: DefaultListBasicTokenSchema.entries.decimals,
+  image: optional(DefaultListBasicTokenSchema.entries.image),
+  mintUrl: pipe(string(), nonEmpty('Please enter a mintUrl'), url()),
+  name: DefaultListBasicTokenSchema.entries.name,
+  protocol: string(),
+  symbol: DefaultListBasicTokenSchema.entries.symbol,
+  type: picklist(['amm', 'cdp', 'perpetuals', 'unknown', 'vault']),
+  underlyingTokens: array(AddressSchema),
+})
+
 export const TokenSchema = union([
-  strictObject({
-    address: AddressSchema,
-    decimals: number(),
-    image: string(),
-    name: string(),
-    symbol: string(),
-  }),
-  strictObject({
-    address: AddressSchema,
-    decimals: number(),
-    image: optional(string()),
-    mintUrl: pipe(string(), nonEmpty('Please enter a mintUrl'), url()),
-    name: string(),
-    protocol: string(),
-    symbol: string(),
-    type: picklist(['amm', 'cdp', 'perpetuals', 'unknown', 'vault']),
-    underlyingTokens: array(AddressSchema),
-  }),
+  DefaultListBasicTokenSchema,
+  DefaultListTokenWithUnderlyingSchema,
 ])
 export type Token = InferOutput<typeof TokenSchema>
 
