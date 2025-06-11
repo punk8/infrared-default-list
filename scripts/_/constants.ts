393e5e262d595795d6b2be088fe9336a07fa6d1f
Mark Lenser <mlenser@gmail.com>
2025-06-11 11:39:33 +0100

    feat: add beratrax and dapdap

--- scripts/_/constants.ts
+++ scripts/_/constants.ts
@@ -1,10 +1,10 @@
 export const ASSETS_FOLDER = 'src/assets'
 export const PROTOCOLS_FOLDER = `${ASSETS_FOLDER}/protocols`
-export const PROTOCOLS_NEW_FOLDER = `${PROTOCOLS_FOLDER}/new`
-export const PROTOCOLS_ORIGINAL_FOLDER = `${PROTOCOLS_FOLDER}/original`
+export const PROTOCOLS_NEW_FOLDER = `${PROTOCOLS_FOLDER}-new`
+export const PROTOCOLS_ORIGINAL_FOLDER = `${PROTOCOLS_FOLDER}-original`
 export const TOKENS_FOLDER = `${ASSETS_FOLDER}/tokens`
-export const TOKENS_NEW_FOLDER = `${TOKENS_FOLDER}/new`
-export const TOKENS_ORIGINAL_FOLDER = `${TOKENS_FOLDER}/original`
+export const TOKENS_NEW_FOLDER = `${TOKENS_FOLDER}-new`
+export const TOKENS_ORIGINAL_FOLDER = `${TOKENS_FOLDER}-original`
 export const BERA_TOKEN_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
 export const IMAGE_SIZE = 128
 export const IMAGE_GAP_BETWEEN = 64
